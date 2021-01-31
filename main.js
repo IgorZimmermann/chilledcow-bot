50ade19864ac41de6c0464120b7f092fba3d1a5d
Szonja Rozsa <igorzimmermann01@gmail.com>
2021-01-31 12:38:51 +0100

    Added auto command reading to main.js

--- main.js
+++ main.js
@@ -6,6 +6,20 @@ const ytdl = require('ytdl-core')
 const config = require('./config/config.json')
 require('dotenv').config()
 
+bot.commands = new discord.Collection()
+bot.aliases = new discord.Collection()
+
+fs.readdir('./commands/', (err, files) => {
+	let jsfiles = files.filter((f) => f.split('.').pop() === 'js')
+	jsfiles.forEach((f, i) => {
+		let props = require(`./commands/${f}`)
+		bot.commands.set(props.help.name, props)
+		props.help.aliases.forEach((alias) => {
+			bot.aliases.set(alias, props.help.name, props)
+		})
+	})
+})
+
 bot.on('ready', () => {
   console.log('Bot is ready...')
   bot.user.setActivity('ChilledCow 24/7', {type: 'LISTENING'})
@@ -27,13 +41,17 @@ bot.on('message', message => {
 	let args = msgArray.splice(1)
 
 	if (cmd.startsWith(bot.prefix)) {
-		let command = cmd.slice(bot.prefix.length)
-		if (command === 'join') {
-      let voiceChannel = message.member.voice.channel
-      if (!voiceChannel) return message.reply('Please join a voice channel first')
-      voiceChannel.join()
-      message.channel.send('Joined the voice channel')
-    }
+		let cmdfile =
+			bot.commands.get(cmd.slice(bot.prefix.length)) ||
+			bot.commands.get(bot.aliases.get(cmd.slice(bot.prefix.length)))
+		if (cmdfile) {
+			remote.emit('bot-command', cmdfile)
+			cmdfile.run(bot, message, args)
+		} else {
+			message.channel.send(
+				`I do not have such command. Use \`${bot.prefix}help\` to see all commands!`
+			)
+		}
 	}
 })
 
